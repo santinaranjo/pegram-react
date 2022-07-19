41addf480ff92836d7dd6d67d289b9d5d00716d5
santinaranjo <marketingsanti@gmail.com>
2022-07-20 01:50:14 +0200

    Implementados styled components

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Anchor, Image } from "./styles";
 
 const DEFAULT_IMAGE = "https://i.imgur.com/dJa0Hpl.jpeg";
 
 const Category = ({ cover = DEFAULT_IMAGE, path, emoji = "?" }) => {
     return(
-        <a href={path}>
-            <img src={cover} />
+        <Anchor href={path}>
+            <Image src={cover} />
             {emoji}
-        </a>
+        </Anchor>
     )
 }
 
