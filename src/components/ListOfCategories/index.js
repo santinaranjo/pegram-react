109f9f9c9a029e32e95ef9f9649f9bb7640dd311
santinaranjo <marketingsanti@gmail.com>
2022-07-20 02:36:33 +0200

    Categorias temporales y test vercel

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { List, Item } from "./styles"
 import { Category } from "../Category"
+import db from "../../../api/db.json"
 
 const ListOfCategories = () => {
     return(
         <List>
             {
-                [1, 2, 3, 4].map(category => <Item key={category}><Category /></Item>)
+                db.categories.map(category => <Item key={category}><Category /></Item>)
             }
         </List>
     )
