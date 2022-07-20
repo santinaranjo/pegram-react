db531c1b600cea331938bb3badd6ee7c56623779
santinaranjo <marketingsanti@gmail.com>
2022-07-21 00:42:25 +0200

    Componente PhotoCard estilizado

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { Img, ImgWrapper, Button } from "./syles";
 
-const PhotoCard = ({id, likes = 0, src}) => {
+const DEFAULT_IMAGE = "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+
+const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
     return(
         <article>
             <a href={`/detail/${id}`}>
-                <div>
-                    <img src={src} />
-                </div>
+                <ImgWrapper>
+                    <Img src={src} />
+                </ImgWrapper>
             </a>
 
-            <button>
+            <Button>
                 {likes} likes!
-            </button>
+            </Button>
         </article>
     )
 }
