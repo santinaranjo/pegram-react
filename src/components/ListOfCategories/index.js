4fc4b0d07d8d6a7e4f7d8e54e1feb10c390e30b5
santinaranjo <marketingsanti@gmail.com>
2022-07-26 00:47:42 +0200

    Lista de categorias scroll

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,23 +2,56 @@ import React, { useEffect } from "react";
 import { List, Item } from "./styles"
 import { Category } from "../Category"
 
-const ListOfCategories = () => {
+function useCategoriesData() {
     const [categories, setCategories] = React.useState([])
-
+    const [loading, setLoading] = React.useState(false)
+    
     useEffect(() => {
+        setLoading(true)
         fetch("https://petgram-server-santinaranjo-santinaranjo.vercel.app/categories")
             .then(response => response.json())
             .then(response => {
                 setCategories(response)
+                setLoading(false)
             })
     }, [])
-    return(
-        <List>
-            {
-                categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
-            }
+
+    return { categories, loading }
+}
+
+const ListOfCategories = () => {
+    const { categories, loading } = useCategoriesData()
+    const [showFixed, setShowFixed] = React.useState(false)
+
+
+
+    useEffect(() => {
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200
+            showFixed !== newShowFixed && setShowFixed(newShowFixed)
+        }
+
+        document.addEventListener("scroll", onScroll)
+
+        return () => document.removeEventListener("scroll", onScroll)
+    }, [showFixed])
+
+    const renderList = (fixed) => (
+        <List fixed={fixed}>
+          {
+            loading
+                ? <Item key="loading"><Category /></Item>
+                : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+          }
         </List>
-    )
+      )
+
+      return (
+        <React.Fragment>
+          {renderList()}
+          {showFixed && renderList(true)}
+        </React.Fragment>
+      )
 }
 
 export { ListOfCategories }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { fadeIn } from "../../styles/animation";
 
 const List = styled.ul`
     display: flex;
@@ -7,6 +8,23 @@ const List = styled.ul`
     &::-webkit-scrollbar {
         display: none;
     }
+    ${props => props.fixed && css`
+    {
+        ${fadeIn({ time: "0.5s" })}
+        background: #fff;
+        border-radius: 60px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        left: 0;
+        margin: 0 auto;
+        max-width: 400px;
+        padding: 5px;
+        position: fixed;
+        right: 0;
+        top: -20px;
+        transform: scale(.5);
+        z-index: 1;
+    }
+    `}
 `
 
 const Item = styled.li`
