7de6665a36155e4fd401e8947f77310a826eb14e
santinaranjo <marketingsanti@gmail.com>
2022-07-27 01:49:13 +0200

    Implementado fetch con GraphQL y ReactApollo

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { PhotoCard } from "../PhotoCard";
+import { useQuery, gql } from "@apollo/client";
+
+const ANIMALS_QUERY = gql`
+    query getPhotos {
+        photos {
+        id
+        categoryId
+        src
+        likes
+        userId
+        liked
+        }
+    }
+`
 
 const ListOfPhotoCards = () => {
-    return(
-        <ul>
-            {[1,2,3,4,5,6,7].map( id => <PhotoCard key={id} id={id} /> )}
-        </ul>
-    )
+    const { data, loading, error } = useQuery(ANIMALS_QUERY)
+  if (loading) return 'Loading...'
+  if (error) return <pre>{error.message}</pre>
+  return (
+    <ul>
+      {data.photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
+    </ul>
+  )
 }
+
 
 export { ListOfPhotoCards }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://petgram-server-santinaranjo-santinaranjo.vercel.app/graphql',
+  cache: new InMemoryCache(),
+});
 
 
 const root = ReactDOM.createRoot(
@@ -8,5 +14,7 @@ const root = ReactDOM.createRoot(
 )
 
 root.render(
-  <App></App>
+  <ApolloProvider client={client}>
+    <App></App>
+  </ApolloProvider>
 )
