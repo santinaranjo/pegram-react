8ac7dcdf7d7d7278c9dc8aa145bf61087444fea0
santinaranjo <marketingsanti@gmail.com>
2022-08-09 21:33:56 +0200

    autenticación de usuario con api pendiente

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Img, ImgWrapper, Article } from "./syles";
-import { useLocalStorage } from "../../hooks/useLocalStorage"
+// import { useLocalStorage } from "../../hooks/useLocalStorage"
 import { useNearScreen } from "../../hooks/useNearScreen"
 import { FavButton } from "../FavButton"
 import { ToggleLikeMutation } from "../../container/ToggleLikeMutation"
@@ -8,10 +8,10 @@ import { Link } from "react-router-dom";
 
 const DEFAULT_IMAGE = "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
 
-const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
+const PhotoCard = ({id, liked, likes = 0, src = DEFAULT_IMAGE}) => {
     const [show, element] = useNearScreen()
-    const key = `like-${id}`
-    const [liked, setLiked] = useLocalStorage(key, false)
+    // const key = `like-${id}`
+    // const [liked, setLiked] = useLocalStorage(key, false)
 
     return(
         <Article ref={element}>
@@ -26,10 +26,10 @@ const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
                                 {
                                     (toggleLike) => {
                                         const handleFavClick = () => {
-                                            !liked && toggleLike({ variables: {
+                                            toggleLike({ variables: {
                                                 input: { id }
                                             } })
-                                            setLiked(!liked)
+                                            // setLiked(!liked)
                                         }
                                         return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
                                     }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, ApolloLink, concat } from '@apollo/client';
 import { AuthProvider } from './context/Auth';
+
+const httpLink = new HttpLink({ uri: 'https://petgram-server-santinaranjo-santinaranjo.vercel.app/graphql' })
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = window.sessionStorage.getItem('token')
+  const authorization = token ? `Bearer ${token}` : ''
+  operation.setContext({
+    headers: {
+      authorization
+    }
+  })
+
+  return forward(operation)
+})
 
 const client = new ApolloClient({
   uri: 'https://petgram-server-santinaranjo-santinaranjo.vercel.app/graphql',
   cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink)
 });
 
 
